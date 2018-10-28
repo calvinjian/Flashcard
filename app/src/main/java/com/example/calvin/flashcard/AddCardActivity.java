59adb0fadb0b514acf2c2d0034197d88094d3cc0
Calvin Jian <cjian1@binghamton.edu>
2018-10-28 14:13:15 -0400

    Lab 2

---
+++ app/src/main/java/com/example/calvin/flashcard/AddCardActivity.java
@@ -0,0 +1,35 @@
+package com.example.calvin.flashcard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        //Set a cancel/close current view button
+        findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        //Set a save button to save input text (Question and Answer)
+        findViewById(R.id.saveBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
+                intent.putExtra("Question", ((EditText) findViewById(R.id.addQuestion)).getText().toString());
+                intent.putExtra( "Answer", ((EditText) findViewById(R.id.addAnswer)).getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/calvin/flashcard/MainActivity.java
+++ app/src/main/java/com/example/calvin/flashcard/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.calvin.flashcard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Set the Flashcard's answer to visible and the Flashcard's question to invisible
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener(){
            @Override
            public void onClick(View v){
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
            }
         });
+        //Set the Flashcard's question to visible and the Flashcard's answer to invisible
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -24,5 +28,23 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
+        //Set Onclicklistener to + icon
+        findViewById(R.id.addBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            String question = data.getExtras().getString("Question");
+            String answer = data.getExtras().getString("Answer");
+            ((TextView) findViewById(R.id.flashcard_question)).setText(question);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
+        }
     }
 }
